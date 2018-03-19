7c090516ab3ab22ca49d6b391a3775ae5d7974fc
jane martin <223120023@student.unaki.ac.id>
2018-03-19 06:57:16 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,47 @@
+def input_to_index(inputNumber)
+  index=inputNumber.to_i-1
+  return index
+end
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+def valid_move?(board,index)
+  if !position_taken?(board,index)&& index.between?(0,8)
+    puts "benar"
+    return true
+  else
+    puts "salah"
+    return false
+  end
+end
+
+def position_taken?(board,index)
+  if board[index]==""||board[index]==" "||board[index]==nil
+    return false
+  elsif board[index]=="X"||board[index]="O"
+    return true
+  end
+end
+def move(board,index,value)
+  if valid_move?(board,index)
+    board[index]=value
+  else
+    puts "salah"
+  end
+end
+def turn(board)
+    puts "Please enter 1-9:"
+    inputNumber = gets.strip
+    index=input_to_index(inputNumber)
+    if valid_move?(board,index)
+      move(board,index,value="X")
+      display_board(board)
+      turn(board)
+    else
+      turn(board)
+    end
+end
